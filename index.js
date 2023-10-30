4f701c1c1a42450314c7910e4a926b82f32700cc
k3113n <kellen@nellek.com>
2023-10-30 00:15:11 -0500

    Initial commit.

---
+++ index.js
@@ -0,0 +1,28 @@
+let server = new (require('ws')).Server({port: 80}),
+    sockets = {},
+    a = 0;
+
+server.on('connection', function (socket) {
+    let user = crypto.randomUUID();
+    sockets[user] = socket;
+    socket.send(a);
+
+    socket.on('message', function (message) {
+        let b = Number(message);
+        if(!isNaN(b)) {
+            if(a < b){
+                a = b;      
+                for(const [i, s] of Object.entries(sockets)) {
+                    if(i != user) s.send(a.toString());
+                }
+            } 
+            if(a > b) {
+                socket.send(a.toString());
+            }
+        }
+    });
+
+    socket.on('close', function () {
+        delete sockets[user];
+    });
+});
